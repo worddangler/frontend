e940ccacc2edad94a2f991a944b6afc2b5926d60
Mayank Tamakuwala <77758563+MayankTamakuwala@users.noreply.github.com>
2023-03-28 17:10:35 -0700

    Create-New-Session Demo
    Added Create-New-Session event in the useEffect. Will be activated once you click on "Create new lobby" on the index page. The new session id will be stored in the local storage of the browser with the key "sessionId"

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,31 @@
-const Index = () => <div>index</div>;
+import { useNavigate } from "react-router-dom";
+
+const Index = () => {
+  const navigate = useNavigate();
+  return (
+    <div>
+      <div>index</div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <button
+          className="btn m-2"
+          onClick={() => {
+            navigate("/play");
+          }}
+        >
+          Create New Lobby
+        </button>
+        <button className="btn m-2">Enter Session ID</button>
+        <button className="btn m-2">Enter Invite Link</button>
+      </div>
+    </div>
+  );
+};
 
 export default Index;
