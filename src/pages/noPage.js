0bc3f6e521d771b0c1954e2cdb9017cb9ab5eb6f
Mayank Tamakuwala <77758563+MayankTamakuwala@users.noreply.github.com>
2023-04-03 14:42:10 -0700

    CSS changes

--- src/pages/noPage.js
+++ src/pages/noPage.js
@@ -3,12 +3,12 @@ import AnimatedTitle from "../components/animatedTitle";
 const NoPage = () => {
   return (
     <div className="justify-center items-center flex flex-1 h-screen">
-      <text className="text-3xl font-medium">
+      <text className="text-3xl font-medium text-center">
         <AnimatedTitle
           title="Error 404"
           className="text-2xl sm:text-4xl lg:text-6xl mt-10 text-red-500"
         />
-        Page Not Found (Inavild Link)
+        Page Not Found <br></br>(Inavild Link)
       </text>
     </div>
   );
